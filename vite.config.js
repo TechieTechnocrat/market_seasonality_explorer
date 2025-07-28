4d75a783736873d746091ad63da25dbeadeba87a
TechieTechnocrat <wtasonalkumari@gmail.com>
2025-07-28 21:15:58 +0530

    Added config

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/market_seasonality_explorer/",
   test: {
     environment: "jsdom",
     globals: true,
-    base: "/market_seasonality_explorer/"
   },
 });
